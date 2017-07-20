e8e13f86c250caea76600e21d82ca434a3ca5248
samnar2017 <samnarrainen1@gmail.com>
2017-07-19 20:30:11 -0400

    Add files via upload

---
+++ first_code.py
@@ -0,0 +1,13 @@
+
+
+
+
+a= int(input("What is the temperature outside ?"))
+def multiple_function():
+    if 60 < a and a < 90:
+        print ("warm")
+    elif a >= 90:
+        print ("hot")
+    elif a < 60:
+        print ("chili")
+multiple_function()
